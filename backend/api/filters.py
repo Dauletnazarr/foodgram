c4dbeb351e30b4fe7692ead8e850796b204e51e5
Dauletnazarr <richdad95@yandex.ru>
2025-01-16 14:39:01 +0300

    asd

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,6 @@
 from django_filters import rest_framework as filters
+from django_filters import CharFilter
+
 from recipes.models import Recipe, Tag
 
 
@@ -12,9 +14,16 @@ class RecipeFilter(filters.FilterSet):
     is_in_shopping_cart = filters.BooleanFilter(
         method="filter_is_in_shopping_cart")
 
+    ingredient_name = CharFilter(
+        field_name="ingredients__name",  # Поле, по которому будет фильтрация
+        lookup_expr="icontains",  # Поиск без учета регистра и по вхождению
+        label="Поиск по названию ингредиента"
+    )
+
     class Meta:
         model = Recipe
-        fields = ("tags", "author", "is_favorited", "is_in_shopping_cart")
+        fields = ("tags", "author", "is_favorited",
+                  "is_in_shopping_cart", "ingredient_name")
 
     def filter_is_favorited(self, queryset, name, value):
         """
