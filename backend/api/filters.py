5c84da15c4d9881f4c78bb0b00ee30b933a6aa58
Dauletnazarr <richdad95@yandex.ru>
2025-01-17 10:49:58 +0300

    вывы

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework as filters
 from django_filters import CharFilter
+from django.db.models import Q
 
 from recipes.models import Recipe, Tag
 
@@ -14,12 +15,7 @@ class RecipeFilter(filters.FilterSet):
     is_in_shopping_cart = filters.BooleanFilter(
         method="filter_is_in_shopping_cart"
     )
-    ingredient_name = CharFilter(
-        field_name="ingredients__name",  # Поле, по которому будет фильтрация
-        lookup_expr="icontains",  # Поиск без учета регистра и по вхождению
-        label="Поиск по названию ингредиента"
-    )
-
+    ingredient_name = CharFilter(method='filter_ingredient_name')
     class Meta:
         model = Recipe
         fields = ("tags", "author", "is_favorited",
@@ -43,3 +39,16 @@ class RecipeFilter(filters.FilterSet):
         if value and user.is_authenticated:
             return queryset.filter(in_cart__user=user)
         return queryset
+
+    def filter_ingredient_name(self, queryset, name, value):
+        """
+        Фильтрация рецептов по названию ингредиентов без учета порядка слов.
+        """
+        # Разделяем запрос на отдельные слова
+        words = value.split()
+        # Формируем фильтры для поиска каждого слова
+        q_objects = Q()
+        for word in words:
+            q_objects &= Q(ingredients__name__icontains=word)
+        # Применяем фильтр к queryset
+        return queryset.filter(q_objects)
