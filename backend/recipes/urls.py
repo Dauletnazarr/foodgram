9432db61f59d27cc68a15245138b29f2aeb8bc70
Dauletnazarr <richdad95@yandex.ru>
2025-01-17 21:26:52 +0300

    sds

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('s/<str:short_link>/', views.redirect_short_link, name='short_link'),
+    path('r/<str:short_link>/', views.redirect_short_link, name='short_link'),
 ]
